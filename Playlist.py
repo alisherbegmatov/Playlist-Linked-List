5b3a555aa774fc038298d99f1cee4b3baadb150d
alisherbegmatov <60242355+alisherbegmatov@users.noreply.github.com>
2021-02-02 14:25:02 -0800

    Code has been indented. Also, added README.md File
    Code has been indented. Also, added README.md File

--- Playlist.py
+++ Playlist.py
@@ -7,17 +7,16 @@ class Playlist:
 
   # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
 
-  # Created add_song Method
+# Created add_song Method
   def add_song(self, title):
     new_song = Song(title)
     new_song.set_next_song(self.__first_song)
     self.__first_song = new_song
 
 
-
   # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
 
-  # Created find_song Method
+# Created find_song Method
   def find_song(self, title):
     current_song = self.__first_song
     index = 0
@@ -25,7 +24,7 @@ class Playlist:
     while current_song != None:
       if current_song.get_title() == title:
         return index
-
+      
       if current_song.get_title() != title:
         index += 1
         current_song = current_song.get_next_song()
@@ -35,7 +34,7 @@ class Playlist:
 
   # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
 
-  #Created remove_song Method
+#Created remove_song Method
   def remove_song(self, title):
     previous_song = None
     current_song = self.__first_song
@@ -60,10 +59,9 @@ class Playlist:
           break
 
 
-
   # TODO: Create a method called length, which returns the number of songs in the playlist.
 
-  # Created length Method
+# Created length Method
   def length(self):
     length = 0
     current_song = self.__first_song
@@ -81,11 +79,11 @@ class Playlist:
   # 2. Song Title 2
   # 3. Song Title 3
 
-  # Created print_songs Method
+# Created print_songs Method
   def print_songs(self):
     current_song = self.__first_song
     index = 1
     while current_song != None:
       print(f"{str(index)}. {current_song}")
       current_song = current_song.get_next_song()
-      index += 1
+      index += 1  
---
+++ README.md
@@ -0,0 +1,7 @@
+# Music Playlist 🎶
+
+# Description
+
+### In this project, I have build a music playlist builder using a linked list as the underlying data structure. My playlist builder allows users to add new songs, remove songs, search for songs, and get the length of the playlist.
+
+# I have used Python programming language for this project.
